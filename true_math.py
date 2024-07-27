c366af1b3f04f66f7118b8c3796fb61796b6e883
ViktorGolikoff <pravgolvik@gmail.com>
2024-07-27 14:24:32 +0900

    Add files via upload

---
+++ true_math.py
@@ -0,0 +1,10 @@
+from math import inf
+def divide(first, second):
+    if second == 0:
+        print(inf)
+        return inf
+    else:
+        print(first/second)
+        return first/second
+
+
